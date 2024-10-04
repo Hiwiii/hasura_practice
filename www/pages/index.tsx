e22de8c14bab2454945232db4d853b628f79324e
Hiwiii <42866660+Hiwiii@users.noreply.github.com>
2024-10-04 13:59:17 +0300

    updated deploy friends

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,32 +1,39 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
+type Friend = {
+  name: string;
+};
 
 type Data = {
-  friends: Record <string, string>[];
-}
+  friends: Friend[];
+};
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  let friends = [];
-  
-  const data = await fetch(process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string, {
-    headers: {
-      'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET as string,
-    },
-    body: JSON.stringify({
-      query: `
-        query {
-          friend {
-            name
+  let friends: Friend[] = [];
+
+  const data = await fetch(
+    process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string,
+    {
+      headers: {
+        "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET as string,
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            friend {
+              name
+            }
           }
-        }
-      `
-    }),
-    method: 'POST',
-  });
+        `,
+      }),
+      method: "POST",
+    }
+  );
 
   const response = await data.json();
-  console.log('Hasura response:', response);
+  console.log("Hasura response:", response);
 
   if (response?.data?.friend) {
     friends = response.data.friend;
@@ -37,24 +44,17 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   };
 };
 
-const Home: NextPage=({friends}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home: NextPage<Data> = ({ friends }) => {
   return (
-    <div
-      
-    >
+    <div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {friends.map((friend) => (
           <div key={friend.name}>{friend.name}</div>
         ))}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
-}
+};
 
 export default Home;
-
-
-
